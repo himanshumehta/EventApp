79e9559d1f80cfc654dfb91eeac9588c05291a82
Himanshu Mehta <mehta.august@gmail.com>
2019-01-04 17:38:43 +0530

    Added models and entity relationship and seeded user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+	User.create(name: FFaker::Name.name, email: FFaker::Internet.email, phone: FFaker::PhoneNumber.phone_number)
+end
